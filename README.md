98cd7cea9f93c3b71069596af5a67afbcccc1559
Chris Arderne <chris@rdrn.me>
2020-07-11 19:00:54 +0200

    Switch from Tabletop to Papaparse

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # leaflet-gsheets
 
-How to create a simple [Leaflet](https://leafletjs.com/) web map that automatically pulls data from two simple Google Sheets tables. Sidebar created using [leaflet-sidebar-v2](https://github.com/nickpeihl/leaflet-sidebar-v2), and Google Sheets connection with [Tabletop.js](https://github.com/jsoma/tabletop).
+How to create a simple [Leaflet](https://leafletjs.com/) web map that automatically pulls data from two simple Google Sheets tables. Sidebar created using [leaflet-sidebar-v2](https://github.com/nickpeihl/leaflet-sidebar-v2), and Google Sheets connection with [PapaParse](https://github.com/mholt/PapaParse).
 
 The resultant map can be seen here: [https://rdrn.me/leaflet-gsheets/](https://rdrn.me/leaflet-gsheets/)
 
@@ -13,9 +13,9 @@ I explained the process in more length in my blog post here: [Leaflet maps with 
     - [Polys](https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing)
     - [Points](https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing)
 2. In each one, do the following three steps:
-   - In Google Sheets, go to File -> Publish to the Web -> Publish (and then close the dialog)
-   - Then click Share in the top right -> Get Shareable Link (ensure "Anyone with the link can view" is enabled)
-   - Copy the link provided and keep it aside
+   - In Google Sheets, go to File -> Publish to the Web -> Publish
+   - Choose "Comma-separated values (.csv)" in the dropdown on the right
+   - Then copy the link provided just underneath, and save it somewhere
 3. Back in GitHub, Fork this repo (top right of this page)
 4. Either clone/download your new repo to your machine, or use GitHub's built-in code editor, and open [leaflet-gsheets.js](leaflet-gsheets.js)
 5. At line 20 where it says "PASTE YOUR URLs HERE", paste the URLs you copied above
--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -1,8 +1,8 @@
-/* global L Tabletop */
+/* global L Papa */
 
 /*
  * Script to display two tables from Google Sheets as point and polygon layers using Leaflet
- * The Sheets are then imported using Tabletop.js and overwrite the initially laded layers
+ * The Sheets are then imported using PapaParse and overwrite the initially laded layers
  */
 
 // init() is called as soon as the page loads
@@ -11,12 +11,20 @@ function init() {
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
   var polyURL =
-    "https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsAyA0Hpk_-WpKyN1dfqi5IPEIC3rqEiL-uwElxJpw_U7BYntc8sDw-8sWsL87JCDU4lVg2aNi65ES/pub?output=csv";
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFQw9sVY16eQmN5TIjOH7CUaxeZnl_v6LcdE2goig1pSe9I3hipeOn1sOwmC4fS0AURefRWwcKExct/pub?output=csv";
 
-  Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
-  Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
+  Papa.parse(polyURL, {
+    download: true,
+    header: true,
+    complete: addPolygons,
+  });
+  Papa.parse(pointsURL, {
+    download: true,
+    header: true,
+    complete: addPoints,
+  });
 }
 window.addEventListener("DOMContentLoaded", init);
 
@@ -30,7 +38,7 @@ var basemap = L.tileLayer(
     attribution:
       "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
     subdomains: "abcd",
-    maxZoom: 19
+    maxZoom: 19,
   }
 );
 basemap.addTo(map);
@@ -39,7 +47,7 @@ var sidebar = L.control
   .sidebar({
     container: "sidebar",
     closeButton: true,
-    position: "right"
+    position: "right",
   })
   .addTo(map);
 
@@ -48,11 +56,11 @@ var panelContent = {
   id: panelID,
   tab: "<i class='fa fa-bars active'></i>",
   pane: "<p id='sidebar-content'></p>",
-  title: "<h2 id='sidebar-title'>No state selected</h2>"
+  title: "<h2 id='sidebar-title'>No state selected</h2>",
 };
 sidebar.addPanel(panelContent);
 
-map.on("click", function() {
+map.on("click", function () {
   sidebar.close(panelID);
 });
 
@@ -60,21 +68,22 @@ map.on("click", function() {
 var polygonLayer;
 var pointGroupLayer;
 
-// The form of data must be a JSON representation of a table as returned by Tabletop.js
+// The form of data must be a JSON representation of a table as returned by PapaParse
 // addPolygons first checks if the map layer has already been assigned, and if so, deletes it and makes a fresh one
-// The assumption is that the locally stored JSONs will load before Tabletop.js can pull the external data from Google Sheets
+// The assumption is that the locally stored JSONs will load before PapaParse can pull the external data from Google Sheets
 function addPolygons(data) {
+  data = data.data;
   if (polygonLayer != null) {
     // If the layer exists, remove it and continue to make a new one with data
     polygonLayer.remove();
   }
 
-  // Need to convert the Tabletop.js JSON into a GeoJSON
+  // Need to convert the PapaParse JSON into a GeoJSON
   // Start with an empty GeoJSON of type FeatureCollection
   // All the rows will be inserted into a single GeoJSON
   var geojsonStates = {
     type: "FeatureCollection",
-    features: []
+    features: [],
   };
 
   for (var row in data) {
@@ -86,14 +95,14 @@ function addPolygons(data) {
         type: "Feature",
         geometry: {
           type: "MultiPolygon",
-          coordinates: coords
+          coordinates: coords,
         },
         properties: {
           name: data[row].name,
           summary: data[row].summary,
           state: data[row].state,
-          local: data[row].local
-        }
+          local: data[row].local,
+        },
       });
     }
   }
@@ -103,15 +112,15 @@ function addPolygons(data) {
   var polygonHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
 
   polygonLayer = L.geoJSON(geojsonStates, {
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       layer.on({
-        mouseout: function(e) {
+        mouseout: function (e) {
           e.target.setStyle(polygonStyle);
         },
-        mouseover: function(e) {
+        mouseover: function (e) {
           e.target.setStyle(polygonHoverStyle);
         },
-        click: function(e) {
+        click: function (e) {
           // This zooms the map to the clicked polygon
           // map.fitBounds(e.target.getBounds());
 
@@ -123,16 +132,17 @@ function addPolygons(data) {
           document.getElementById("sidebar-content").innerHTML =
             e.target.feature.properties.summary;
           sidebar.open(panelID);
-        }
+        },
       });
     },
-    style: polygonStyle
+    style: polygonStyle,
   }).addTo(map);
 }
 
 // addPoints is a bit simpler, as no GeoJSON is needed for the points
 // It does the same check to overwrite the existing points layer once the Google Sheets data comes along
 function addPoints(data) {
+  data = data.data;
   if (pointGroupLayer != null) {
     pointGroupLayer.remove();
   }
@@ -153,9 +163,13 @@ function addPoints(data) {
   for (var row = 0; row < data.length; row++) {
     var marker;
     if (markerType == "circleMarker") {
-      marker = L.circleMarker([data[row].lat, data[row].lon], {radius: markerRadius});
+      marker = L.circleMarker([data[row].lat, data[row].lon], {
+        radius: markerRadius,
+      });
     } else if (markerType == "circle") {
-      marker = L.circle([data[row].lat, data[row].lon], {radius: markerRadius});
+      marker = L.circle([data[row].lat, data[row].lon], {
+        radius: markerRadius,
+      });
     } else {
       marker = L.marker([data[row].lat, data[row].lon]);
     }
@@ -168,18 +182,18 @@ function addPoints(data) {
     marker.feature = {
       properties: {
         location: data[row].location,
-        category: data[row].category
-      }
+        category: data[row].category,
+      },
     };
     marker.on({
-      click: function(e) {
+      click: function (e) {
         L.DomEvent.stopPropagation(e);
         document.getElementById("sidebar-title").innerHTML =
           e.target.feature.properties.location;
         document.getElementById("sidebar-content").innerHTML =
           e.target.feature.properties.category;
         sidebar.open(panelID);
-      }
+      },
     });
 
     // AwesomeMarkers is used to create fancier icons
@@ -188,7 +202,7 @@ function addPoints(data) {
       iconColor: "white",
       markerColor: getColor(data[row].category),
       prefix: "glyphicon",
-      extraClasses: "fa-rotate-0"
+      extraClasses: "fa-rotate-0",
     });
     if (!markerType.includes("circle")) {
       marker.setIcon(icon);
