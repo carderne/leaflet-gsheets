18671b5c74aa6f4cc676a5ffc75b829976d3043a
Chris Arderne <chris@rdrn.me>
2020-07-11 20:25:53 +0200

    Clean up js and requirements a little

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ function init() {
   // PASTE YOUR URLs HERE
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
-  var polyURL =
+  let polyURL =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsAyA0Hpk_-WpKyN1dfqi5IPEIC3rqEiL-uwElxJpw_U7BYntc8sDw-8sWsL87JCDU4lVg2aNi65ES/pub?output=csv";
-  var pointsURL =
+  let pointsURL =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFQw9sVY16eQmN5TIjOH7CUaxeZnl_v6LcdE2goig1pSe9I3hipeOn1sOwmC4fS0AURefRWwcKExct/pub?output=csv";
 
   Papa.parse(polyURL, {
@@ -29,10 +29,10 @@ function init() {
 window.addEventListener("DOMContentLoaded", init);
 
 // Create a new Leaflet map centered on the continental US
-var map = L.map("map").setView([40, -100], 4);
+let map = L.map("map").setView([40, -100], 4);
 
 // This is the Carto Positron basemap
-var basemap = L.tileLayer(
+let basemap = L.tileLayer(
   "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png",
   {
     attribution:
@@ -43,7 +43,7 @@ var basemap = L.tileLayer(
 );
 basemap.addTo(map);
 
-var sidebar = L.control
+let sidebar = L.control
   .sidebar({
     container: "sidebar",
     closeButton: true,
@@ -52,7 +52,7 @@ var sidebar = L.control
   .addTo(map);
 
 let panelID = "my-info-panel";
-var panelContent = {
+let panelContent = {
   id: panelID,
   tab: "<i class='fa fa-bars active'></i>",
   pane: "<p id='sidebar-content'></p>",
@@ -65,8 +65,8 @@ map.on("click", function () {
 });
 
 // These are declared outisde the functions so that the functions can check if they already exist
-var polygonLayer;
-var pointGroupLayer;
+let polygonLayer;
+let pointGroupLayer;
 
 // The form of data must be a JSON representation of a table as returned by PapaParse
 // addPolygons first checks if the map layer has already been assigned, and if so, deletes it and makes a fresh one
@@ -81,15 +81,15 @@ function addPolygons(data) {
   // Need to convert the PapaParse JSON into a GeoJSON
   // Start with an empty GeoJSON of type FeatureCollection
   // All the rows will be inserted into a single GeoJSON
-  var geojsonStates = {
+  let geojsonStates = {
     type: "FeatureCollection",
     features: [],
   };
 
-  for (var row in data) {
+  for (let row in data) {
     // The Sheets data has a column 'include' that specifies if that row should be mapped
     if (data[row].include == "y") {
-      var coords = JSON.parse(data[row].geometry);
+      let coords = JSON.parse(data[row].geometry);
 
       geojsonStates.features.push({
         type: "Feature",
@@ -108,8 +108,8 @@ function addPolygons(data) {
   }
 
   // The polygons are styled slightly differently on mouse hovers
-  var polygonStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 1.5 };
-  var polygonHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
+  let polygonStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 1.5 };
+  let polygonHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
 
   polygonLayer = L.geoJSON(geojsonStates, {
     onEachFeature: function (feature, layer) {
@@ -153,15 +153,15 @@ function addPoints(data) {
   // marker: standard point with an icon
   // circleMarker: a circle with a radius set in pixels
   // circle: a circle with a radius set in meters
-  var markerType = "marker";
+  let markerType = "marker";
 
   // Marker radius
   // Wil be in pixels for circleMarker, metres for circle
   // Ignore for point
-  var markerRadius = 100;
+  let markerRadius = 100;
 
-  for (var row = 0; row < data.length; row++) {
-    var marker;
+  for (let row = 0; row < data.length; row++) {
+    let marker;
     if (markerType == "circleMarker") {
       marker = L.circleMarker([data[row].lat, data[row].lon], {
         radius: markerRadius,
@@ -197,11 +197,11 @@ function addPoints(data) {
     });
 
     // AwesomeMarkers is used to create fancier icons
-    var icon = L.AwesomeMarkers.icon({
-      icon: "info-sign",
+    let icon = L.AwesomeMarkers.icon({
+      icon: "info-circle",
       iconColor: "white",
       markerColor: getColor(data[row].category),
-      prefix: "glyphicon",
+      prefix: "fa",
       extraClasses: "fa-rotate-0",
     });
     if (!markerType.includes("circle")) {
@@ -212,13 +212,14 @@ function addPoints(data) {
 
 // Returns different colors depending on the string passed
 // Used for the points layer
+/*eslint indent: [2, 2, {"SwitchCase": 1}]*/
 function getColor(type) {
   switch (type) {
-  case "Coffee Shop":
-    return "green";
-  case "Restaurant":
-    return "blue";
-  default:
-    return "green";
+    case "Coffee Shop":
+      return "green";
+    case "Restaurant":
+      return "blue";
+    default:
+      return "green";
   }
 }
