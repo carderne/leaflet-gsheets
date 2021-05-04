3661b6cc9ee2fc6b0a4f1333ac1d41c53b0a7be3
Chris Kontos <72415104+kontoschris@users.noreply.github.com>
2021-05-04 09:02:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,6 @@ function addGeoms(data) {
         el.properties = {
           name: data[row].name,
           description: data[row].description,
-          lat: "dsdd",
         };
         fc.features.push(el);
       });
@@ -116,7 +115,7 @@ function addGeoms(data) {
         click: function (e) {
           // This zooms the map to the clicked geometry
           // Uncomment to enable
-          // map.fitBounds(e.target.getBounds());
+          map.fitBounds(e.target.getBounds());
 
           // if this isn't added, then map.click is also fired!
           L.DomEvent.stopPropagation(e);
